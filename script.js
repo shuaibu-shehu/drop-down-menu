7ff50195ab2db15eebd03fe82393123f26d26692
shuaibu-shehu <shuaibushehukalifa@gmail.com>
2023-06-04 17:28:11 +0530

    added some files

---
+++ script.js
@@ -0,0 +1,8 @@
+let menutoggle=document.querySelector('.menutoggle');
+let navigation=document.querySelector('.navigation');
+
+
+menutoggle.addEventListener('click', function(){
+    navigation.classList.toggle('active');
+})
+
